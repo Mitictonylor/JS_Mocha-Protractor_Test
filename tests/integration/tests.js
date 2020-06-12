3748b843e5b8a6376d227e1cc8102ed228e0eef3
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-12 15:31:14 +0100

    starting point + 2 test passing

---
+++ tests/integration/tests.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe('calculator functionality', function() {
+  beforeEach(function() {
+    browser.ignoreSynchronization = true;
+    browser.get('http://localhost:3000');
+  });
+
+  // write integration tests here in the form of "it should do something..."
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  });
+
+  it('should add 2 + 2', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+
+  })
+
+});
---
+++ tests/unit/calculator_spec.js
@@ -0,0 +1,18 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  })
+ it('it can add', function(){
+   calculator.previousTotal = 1
+   calculator.add(4)
+   assert.equal(5, calculator.runningTotal)
+ })
+});
