ca8d3de79551d82f30a292725d0a71db807b6b47
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-12 17:43:05 +0100

    integration test on decimal numbers

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -61,4 +61,12 @@ describe('calculator functionality', function() {
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('-4')
   })
+  it('should work for decimals numbers', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number4')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0.5')
+  })
 });
