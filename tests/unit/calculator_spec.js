78e6575587eb0a7b2521c88f0ec04e136f18e000
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-12 17:32:12 +0100

    fix the naming

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -11,42 +11,42 @@ describe('calculator', function () {
     assert.equal(true, true)
   });
 
- it('it can add', function(){
+ it('it should add', function(){
    calculator.previousTotal = 4
    calculator.add(1)
    assert.equal(5, calculator.runningTotal)
  });
- it('it can subtract', function() {
+ it('it should subtract', function() {
    calculator.previousTotal = 7;
    calculator.subtract(4);
    assert.equal(3,calculator.runningTotal);
  });
- it('it can multiply', function() {
+ it('it should multiply', function() {
    calculator.previousTotal = 3;
    calculator.multiply(5);
    assert.equal(15, calculator.runningTotal);
  });
- it('it can divide', function() {
+ it('it should divide', function() {
    calculator.previousTotal = 21;
    calculator.divide(7);
    assert.equal(3,calculator.runningTotal);
  });
- it('it can concatenate the clicked numbers to the running total', function() {
+ it('it should concatenate the clicked numbers to the running total', function() {
    calculator.numberClick(6);
    calculator.numberClick(9);
    assert.equal(69,calculator.runningTotal);
  });
- it('it can click on the operator', function() {
+ it('it should click on the operator', function() {
    calculator.operatorClick('+');
    assert.equal(true, calculator.newTotal);
  })
- it('it can  clear total when clearClick is pushed', function() {
+ it('it should  clear total when clearClick is pushed', function() {
    calculator.runningTotal = 99;
    calculator.clearClick();
    assert.equal(0, calculator.runningTotal);
  })
 
- it('it can clear previousOperator', function(){
+ it('it should clear previousOperator', function(){
  calculator.previousOperator = '-';
  calculator.clearClick();
  assert.equal(null, calculator.previousOperator);
